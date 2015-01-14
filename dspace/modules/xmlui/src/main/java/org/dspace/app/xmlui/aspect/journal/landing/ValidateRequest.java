6591ef14d5ad6059f288c2066ae4b85fd7c6838b
Nathan Day <rnathanday@yahoo.com>
2015-01-14 01:13:51 +0000

    add logger

--- dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/journal/landing/ValidateRequest.java
+++ dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/journal/landing/ValidateRequest.java
@@ -7,10 +7,14 @@ package org.dspace.app.xmlui.aspect.journal.landing;
 
 import java.util.Map;
 import java.util.HashMap;
+
 import org.apache.avalon.framework.parameters.Parameters;
 import org.apache.cocoon.acting.AbstractAction;
 import org.apache.cocoon.environment.Redirector;
 import org.apache.cocoon.environment.SourceResolver;
+
+import org.apache.log4j.Logger;
+
 import static org.dspace.app.xmlui.aspect.journal.landing.Const.*;
 
 /**
@@ -18,6 +22,9 @@ import static org.dspace.app.xmlui.aspect.journal.landing.Const.*;
  * @author Nathan Day
  */
 public class ValidateRequest extends AbstractAction {
+        
+    private static final Logger log = Logger.getLogger(ValidateRequest.class);
+    
     @Override
     public Map act(Redirector redirector, SourceResolver resolver, Map objectModel, 
                     String source, Parameters parameters) throws Exception 
