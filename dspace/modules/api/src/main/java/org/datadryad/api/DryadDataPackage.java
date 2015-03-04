dd72aca670057560438c35a50b5e9233a2b1208f
Ryan Scherle <ryan@scherle.org>
2015-03-04 11:34:42 -0500

    Correct merge issue with getPublicationName

--- dspace/modules/api/src/main/java/org/datadryad/api/DryadDataPackage.java
+++ dspace/modules/api/src/main/java/org/datadryad/api/DryadDataPackage.java
@@ -291,11 +291,6 @@ public class DryadDataPackage extends DryadObject {
     }
 
 
-    public String getPublicationName() throws SQLException {
-        return getSingleMetadataValue(PUBLICATION_NAME_SCHEMA, PUBLICATION_NAME_ELEMENT, PUBLICATION_NAME_QUALIFIER);
-    }
-
-    
     public void setPublicationName(String publicationName) throws SQLException {
         addSingleMetadataValue(Boolean.TRUE, PUBLICATION_NAME_SCHEMA, PUBLICATION_NAME_ELEMENT, PUBLICATION_NAME_QUALIFIER, publicationName);
     }
